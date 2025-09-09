e499f05a2ede02964c3b072ebc37d65adb9aecb7
hassan-alphasquad <hassanali.alphasquad@gmail.com>
2025-09-09 13:35:31 +0500

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Signin from "./Sign In/page";
+import Signin from "./SignIn/page";
 
    ;
 
@@ -7,7 +7,7 @@ export default function RootLayout({ children }) {
   return (
       <html lang="en">
       <body>
-        <SignIn/>
+        <Signin/>
         {children}
       </body>
     </html>
