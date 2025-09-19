97ad17aaed5530c099fc28f11ea0ee75567fecdb
hassan-alphasquad <hassanali.alphasquad@gmail.com>
2025-09-19 20:52:06 +0500

    commit your message

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,6 @@
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
-   "@autoprefixer": {},
   },
 };
 export default config;
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
